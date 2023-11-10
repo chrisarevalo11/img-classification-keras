df158f6599f75254fe7b12dd40f67e05a7d7df0c
Christian Arevalo <tiancris200@gmail.com>
2023-11-10 08:23:58 -0500

    :art: Updated the image uploading process

--- app/main.py
+++ app/main.py
@@ -1,69 +1,46 @@
-import cv2
-import os
-from flask import Flask, request, jsonify, flash
+import io
+import base64
+import numpy as np
+from flask import Flask, request, jsonify
+from PIL import Image
 from flask_cors import CORS
-from werkzeug.utils import secure_filename
+from model import classify_image
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-UPLOAD_FOLDER = os.path.join('app', 'static')
-
-# Ensure the target directory exists, or create it if it doesn't
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app) 
 app.secret_key = 'secret key' 
 
 @app.route('/')
 def hello_world():
     return 'Hello, Flask!'
 
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def upload_image():
-    if 'image' not in request.files:
-        flash('No file part')
-        return jsonify({'error': "media not provided"}), 400
-    
-    img = request.files['image']
-    if img.filename == '':
-        return jsonify({'error': "image not selected"}), 400
-    
-    if img and allowed_file(img.filename):
-        filename = secure_filename(img.filename)
         
-        # Use os.path.join to create the path and then replace backslashes with forward slashes
-        img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    try:
+        req = request.get_json(force=True)
+
+        image = decode_request(req)
+        batch = preprocess(image)
+
+        prediction = classify_image(batch)
+
+        return jsonify({'prediction': prediction}), 200
+    except:
+        return jsonify({'prediction': 'error'}), 400
+
+def decode_request(req):
+    encoded = req["image"]
+    decoded = base64.b64decode(encoded)
+    return decoded
+
+def preprocess(decoded):
+    pil_image = Image.open(io.BytesIO(decoded)).resize((160,160)).convert("RGB") 
+
+    image = np.asarray(pil_image)
+    batch = np.expand_dims(image, axis=0)
     
-        return process_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-def process_image(img_path):
-    print("img_path:", img_path)
-    test(img_path)
-    return jsonify({'message': "Image uploaded and processed"}), 200
-
-def test(img_path):
-    image = cv2.imread(img_path)
-    print("image:", image)
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return batch
 
 if __name__ == '__main__':
     app.run()
-
-
-
-# try:
-    #     image = cv2.imread(img_path)
-    #     print("image:", image)
-
-    #     prediction = classify_image(image)
-
-    #     return jsonify({'prediction': prediction}), 200
-    # except Exception as e:
-    #     error_message = str(e)  
-    #     app.logger.error(f"An error occurred: {error_message}")
-    #     return jsonify({'error': "invalid image"}), 400
--- app/model.py
+++ app/model.py
@@ -1,12 +1,10 @@
 import tensorflow as tf
-import cv2
 import os
 
 inception_net = tf.keras.models.load_model(os.path.join('app\models', 'final_model.h5'))
 
-labels = ['bicicleta', 'camioneta', 'carro', 'moto']
+labels = ['bicycle', 'van', 'car', 'motorcycle']
 def classify_image(inp):
-    inp = cv2.resize(inp, (160, 160))
     inp = inp.reshape((-1, 160, 160, 3))
 
     prediction = inception_net.predict(inp).flatten()
